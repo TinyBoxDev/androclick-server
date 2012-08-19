f2b4eaa72d77499db83ab537963de57d709c75ac
Pasquale Boemio <boemianrapsodi@gmail.com>
2012-08-19 14:53:27 +0200

    port issue

--- AndroclickServer/src/run_server_wsgi.py
+++ AndroclickServer/src/run_server_wsgi.py
@@ -1,8 +1,10 @@
 from wsgiref.simple_server import make_server
 from AndroclickCore import AndroclickCore;
 from AndroclickCore.AndroclickProperties import AndroclickProperties;
+import os;
 
 androclickServer = AndroclickCore();
 androclickProperties = AndroclickProperties();
-httpd = make_server('0.0.0.0', 50233, androclickServer);
+port = int(os.environ.get('PORT', 5000));
+httpd = make_server('0.0.0.0', port, androclickServer);
 httpd.serve_forever();
