cfff4209c6b35623afb467af82b74781d8f558fe
Pasquale Boemio <boemianrapsodi@gmail.com>
2015-03-27 13:42:01 +0100

    Backbone

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+/**
+ *
+ *  _____ _            ______            ______ 
+ * |_   _(_)           | ___ \           |  _  \
+ *   | |  _ _ __  _   _| |_/ / _____  __ | | | |_____   __
+ *   | | | | '_ \| | | | ___ \/ _ \ \/ / | | | / _ \ \ / /
+ *   | | | | | | | |_| | |_/ / (_) >  <  | |/ /  __/\ V /
+ *   \_/ |_|_| |_|\__, \____/ \___/_/\_\ |___/ \___| \_/
+ *                __/ |
+ *               |___/
+ *
+ * Gruntfile for Androclick Server
+ *
+ **/
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    srcFiles: [ 'lib/**/*.js' ],
+    testFiles: [ 'test/**/*.test.js' ],
+    allFiles: [ '<%= srcFiles %>', '<%= testFiles %>', 'Gruntfile.js' ],
+
+    jshint: {
+      files: ['<%= allFiles %>']
+    },
+
+    mochaTest: {
+      unit: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['<%= testFiles %>']
+      }
+    },
+
+    watch: {
+      files: ['<%= allFiles %>'],
+      tasks: ['test']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('test', ['jshint', 'mochaTest:unit']);
+};
---
+++ test/index.test.js
@@ -0,0 +1,11 @@
+var mocha = require('mocha');
+var chai = require('chai');
+var should = chai.should();
+
+describe('The index module', function(){
+
+  it('should pass', function(done) {
+    done();
+  });
+
+});
